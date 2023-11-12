632b63c2153ad837ef7c017827700b1dae2117ca
KaikyBruno <136467342+KaikyBruno@users.noreply.github.com>
2023-11-11 21:32:57 -0300

    create

--- script.js
+++ script.js
@@ -22,3 +22,40 @@ function voltarbloqueado() {
 
 
 }
+
+function info( ) {
+
+    const formula = document.getElementById ('formula')
+
+    formula.style.display = 'block'
+
+}
+function infoquit () {
+
+    const formula = document.getElementById ('formula')
+
+    formula.style.display = 'none'
+
+}
+
+function Errar () {
+    const pergunta1 = document.getElementById ('pergunta1')
+    const erro = document.getElementById ('erro')
+
+    pergunta1.style.display = 'none'
+    erro.style.display = 'block'
+}
+
+function TentarNovamente () {
+    const pergunta1 = document.getElementById ('pergunta1')
+    const erro = document.getElementById ('erro')
+
+    pergunta1.style.display = 'block'
+    erro.style.display = 'none'
+}
+function acerto1 () {
+    const pergunta1 = document.getElementById ('pergunta1')
+    
+
+    pergunta1.style.display = 'none'
+}
